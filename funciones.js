68ad0183308fc5e30ccbf15e78b8610c0fc44bc5
Seba8117 <81112332+Seba8117@users.noreply.github.com>
2021-04-28 22:44:36 -0400

    funciones calculadoras hechas

---
+++ funciones.js
@@ -0,0 +1,29 @@
+function sumar() {
+    var x = parseInt(document.getElementById('val1').value);
+    var y = parseInt(document.getElementById('val2').value);
+    document.getElementById('resultado').innerHTML = x+y;
+
+
+}
+
+function restar() {
+    var x = parseInt(document.getElementById('val1').value);
+    var y = parseInt(document.getElementById('val2').value);
+    document.getElementById('resultado').innerHTML = x-y;
+
+
+}
+function multiplicar() {
+    var x = parseInt(document.getElementById('val1').value);
+    var y = parseInt(document.getElementById('val2').value);
+    document.getElementById('resultado').innerHTML = x*y;
+
+
+}
+function dividir(){
+    var x = parseInt(document.getElementById('val1').value);
+    var y = parseInt(document.getElementById('val2').value);
+    document.getElementById('resultado').innerHTML = x/y;
+
+
+}
